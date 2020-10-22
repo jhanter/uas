c5762f6c40e89815dfa794f4676d7d6604d447cd
singa37 <singa37@gmail.com>
2020-10-22 19:07:48 +0700

    comit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
-function myFunction() {
+function menu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
+  }
+}
+
+
+window.onscroll = function() {skrull()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function skrull(){
+  if (window.pageYOffset > sticky){
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
